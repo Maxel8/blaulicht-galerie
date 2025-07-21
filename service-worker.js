96399b1db9704162a915651dc4066d51c46d8200
Maximilian <maximilian.nink@web.de>
2025-07-21 15:41:55 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("pwa-cache-v1").then(cache => {
+      return cache.addAll([
+        "/",
+        "/galerie.html",
+        "/RW-Köln.JPEG",
+        "/DLK-Köln.PNG",
+        "/StLF/StLf_1b.JPEG",
+        "/BePolv2-mitBL.png",
+        "/manifest.json",
+        "/service-worker.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
